53bf7860c2d90f578697459c93018dee2aa643ae
Your Name <you@example.com>
2023-04-07 15:50:27 +0300

    task solved

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let result = [...arr];
+    function compare(a, b) {
+		return a.localeCompare(b, "ru", { caseFirst: 'upper' });	
+	}	
 
+    if(param == 'asc') {
+      return result.sort(compare);
+    } else {
+      return result.sort(compare).reverse();
+	} 
 }
