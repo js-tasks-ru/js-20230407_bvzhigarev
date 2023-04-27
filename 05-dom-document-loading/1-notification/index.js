ea42d2f8d9b99a0b50eb2ee1579f79107cb36c1e
Your Name <you@example.com>
2023-04-27 14:46:37 +0300

    partial solution, does not pass tests but works, to be reworked

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,58 @@
 export default class NotificationMessage {
 
+    constructor(text, {
+        duration = 0,
+        type = '',
+    } = {}) {
+        this.duration = duration;
+        this.type = type;
+        this.text = text;
+    }
+
+    get template() {
+        return `
+            <div class="notification ${this.type}" style="--value:20s" id="notificationMessage">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.text}
+                    </div>
+                </div>
+            </div>
+        `
+    }
+    
+    render() {
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = this.template;
+        this.element = wrapper.firstElementChild;
+        document.body.append(this.element);
+        window.setTimeout(this.remove, this.duration);
+        //console.log(this.element === document.getElementById('notificationMessage'));
+    }
+
+    show() {
+        if(document.getElementById('notificationMessage')) {
+            document.getElementById('notificationMessage').remove();
+        }
+        this.render();
+    }
+
+    hide() {
+
+    }
+
+    remove() {
+        document.getElementById('notificationMessage').remove()
+        //if(this.element) {
+        //    this.element.remove();
+        //}
+    }
+    
+    destroy() {
+        this.remove();
+        this.element = {};
+        this.subElements = {};
+    }
 }
